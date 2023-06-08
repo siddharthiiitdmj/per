38d63efb4d7e59ef7ff3aba1edbc93e38bde416b
Sparsh Kumar <76520495+sparshkr24@users.noreply.github.com>
2023-06-09 02:36:58 +0530

    image upload feature

--- src/pages/api/fakedata/getusers.ts
+++ src/pages/api/fakedata/getusers.ts
@@ -3,10 +3,35 @@ import { NextApiRequest, NextApiResponse } from 'next/types'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const users = await prisma.user.findMany()
+    if (req.method === 'POST') {
+      const { email, imgSrc } = req.body
 
-    res.status(200).json(users)
+      // Find the user with the provided email
+      const user = await prisma.user.findUnique({
+        where: { email }
+      })
+
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' })
+      }
+
+      // Update the image field of the user
+      const updatedUser = await prisma.user.update({
+        where: { email },
+        data: { image: imgSrc }
+      })
+
+      return res.status(200).json(updatedUser)
+    }
+
+    if (req.method === 'GET') {
+      const users = await prisma.user.findMany()
+
+      return res.status(200).json(users)
+    }
   } catch (err) {
-    res.status(500).send(err)
+    console.error(err)
+
+    return res.status(500).json({ message: 'Server error' })
   }
 }
