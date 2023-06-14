5d9d961fd341ec02b29690ee7019d2ab66803d22
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-06-14 18:35:42 +0530

    Add id type in next auth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,6 +4,7 @@ import { DefaultSession } from 'next-auth'
 declare module 'next-auth/jwt' {
   interface JWT {
     role: string
+    id: string
   }
 }
 
@@ -11,10 +12,12 @@ declare module 'next-auth' {
   interface Session {
     user: {
       role: string
+      id: string
     } & DefaultSession['user']
   }
 
   interface User {
     role: string
+    id: string
   }
 }
