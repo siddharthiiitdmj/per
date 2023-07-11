504685f590e95e3ab43516de14dee4ff81529919
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-07-11 09:30:51 +0530

    Add store for User Specific Events

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,6 +7,7 @@ import deviceSpecificEvents from './apps/deviceSpecificEvents'
 import uniqueUsers from './apps/uniqueUsers'
 import pieStats from './apps/pieStats'
 import lineStats from './apps/lineStats'
+import userSpecificEvents from './apps/userSpecificEvents'
 
 export const store = configureStore({
   reducer: {
@@ -15,7 +16,8 @@ export const store = configureStore({
     deviceSpecificEvents,
     uniqueUsers,
     pieStats,
-    lineStats
+    lineStats,
+    userSpecificEvents,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
