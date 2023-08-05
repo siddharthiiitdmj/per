68bcea42295927200e2c456800b32c42888fdef2
unknown <sparsh24112002@gmail.com>
2023-08-06 02:22:20 +0530

    Added Last location of user in userDetail page

--- src/pages/api/devices/single.ts
+++ src/pages/api/devices/single.ts
@@ -13,6 +13,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       }
     })
 
+    // const latestEvent = await prisma.events.findFirst({
+    //   where: {
+    //     deviceId: id
+    //   },
+    //   orderBy: {
+    //     updatedAt: 'desc'
+    //   }
+    // })
+
+    // const lat = latestEvent?.Latitude.toFixed(4) || null
+    // const lon = latestEvent?.Longitude.toFixed(4) || null
+    // const location = lat && lon ? `{${lat}, ${lon}}` : null
+
+    // // Check if a user was found
+    // if (device.length > 0) {
+    //   device[0].location = location // Append the location property to the user object
+    // }
+
     res.status(200).json({ data: device, total: device.length })
   } catch (err) {
     res.status(500).send(err)
--- src/pages/api/users/single.ts
+++ src/pages/api/users/single.ts
@@ -8,9 +8,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const user = await prisma.user.findMany({
       where: {
-        altUserId : parsedId
+        altUserId: parsedId
       }
     })
+
+    const latestEvent = await prisma.events.findFirst({
+      where: {
+        userId: parsedId
+      },
+      orderBy: {
+        updatedAt: 'desc'
+      }
+    })
+
+    const lat = latestEvent?.Latitude.toFixed(4) || null
+    const lon = latestEvent?.Longitude.toFixed(4) || null
+    const location = lat && lon ? `{${lat}, ${lon}}` : null
+
+    // Check if a user was found
+    if (user.length > 0) {
+      user[0].location = location // Append the location property to the user object
+    }
 
     res.status(200).json({ data: user, total: user.length })
   } catch (err) {
